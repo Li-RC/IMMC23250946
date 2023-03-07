00624023ca510cb21f96fd47ca464d14eff6b608
tioibiy <1092324959@qq.com>
2023-03-07 13:23:05 +0800

    new file:   Code/C++/test.cpp

---
+++ Code/C++/test.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define LL long long
+#define DB double
+#define IT set<node>::iterator
+using namespace std;
+
+
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
